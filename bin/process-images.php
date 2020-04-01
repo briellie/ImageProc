e55c8f932a3f034a2466f4ad28e6899396706969
Brielle <bruns@2mbit.com>
2020-04-01 12:48:09 -0600

    Small update to preserve ICC profile

--- bin/process-images.php
+++ bin/process-images.php
@@ -7,12 +7,13 @@ include_once("config.inc.php");
 //autorotate($img);
 //$img->stripImage(); // if you want to get rid of all EXIF data
 //$img->writeImage();
-$progVersion="v2.0-PHP";
+$progVersion="v2.0.1-PHP";
 $phpVersion=phpversion();
+$imagickVersion=imagick::getVersion();
 $rawDir="raw/";
 $outDir="resized/";
 
-echo "ImageProc ".$progVersion." - PHP ".$phpVersion."\n\n\n";
+echo "ImageProc ".$progVersion."\nPHP ".$phpVersion."\n".$imagickVersion['versionString']."\n\n\n";
 
 $dateInput=readline("Current Date (press enter for ".date('mdy')."): ");
 $startNumInput=readline("Starting Number: ");
@@ -29,11 +30,17 @@ foreach(glob($rawDir.'*.{jpg,JPG,jpeg,JPEG}',GLOB_BRACE) as $filename){
     $outImgFile=sprintf("%s/%s-%04d.jpg",$outDir, $dateInput, $startNumInput);
     echo "#";
     $img=new Imagick($filename);
+    $iccProfiles = $img->getImageProfiles("icc", true);
     autorotate($img);
     $img->stripImage();
+    if(!empty($profiles)) {
+        $img->profileImage("icc", $iccProfiles['icc']);
+    }
     // This is kinda slow sometimes, so we'll try using scaleImage instead
     //$img->resizeImage($imgWidth, $imgHeight, imagick::FILTER_CATROM, 1);
+    $img->setImageDepth(8);
     $img->scaleImage($imgWidth, $imgHeight, FALSE);
+    $img->setImageCompression(Imagick::COMPRESSION_JPEG);
     $img->setImageCompressionQuality($imgQuality);
     $img->writeImage($outImgFile);
     $img->destroy();
